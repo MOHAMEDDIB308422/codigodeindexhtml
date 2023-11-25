45f92c43e2241f4108092928dbbdf025c9bd3b04
Mohamed <mhdib308422@gmail.com>
2023-11-25 09:54:26 -0400

    hola

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+    let tipoNumero =7.6768766
+    let tipecadenaDeTexto = 'hola mundo'
+    let tipoBooleano = false
+    let tipoUndefinido =undefined
+    let tipoNulo = null
+
+
+let tipoLista = [
+    7,
+    60,
+    54,
+    3,
+    0,
+    23,
+    "hola",
+    "acomos",
+    undefined
+]
+let tipoObjeto ={
+     0:7,
+     1:60,
+     100:1915,
+     name:"mohamed",
+     password:"1234",
+     address:{
+        city:"valencia",
+        country:"venezuela"
+
+     }
+     
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+let MifuncionFlecha =
+(num1,num2,num3)=>{ return num1
+}
+
+
+console.log(MifuncionFlecha)
+console.log(7)
